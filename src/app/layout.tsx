0b26afb8f9278065fc2e0688930a77d28427c995
Anubhab Patnaik <anubhabr50@gmail.com>
2025-02-09 00:54:43 -0500

    style: simplify app title in metadata for clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Neighborly: Your Community, You build it",
+  title: "Neighborly",
   description: "Neighborly: Your Community, You build it",
 };
 
