897fbedaf2519b4a57ee1b1b86f9132469e7dc6d
Aakar Mutha <aakar.mutha18@gmail.com>
2025-02-09 01:43:04 -0500

    added lock and unlock fridge apis

---
+++ src/app/api/unlock/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import clientPromise from '@/lib/mongodb';
+import { ObjectId } from 'mongodb';
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        console.log(request)
+        const fridgeId = body.fridgeId;
+        console.log(fridgeId);
+        if (!fridgeId) {
+            return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+        }
+
+        const client = await clientPromise;
+        const db = client.db('nyuhack');
+        const collection = db.collection('fridge');
+        const result = await collection.updateOne(
+            { _id: new ObjectId(fridgeId) },
+            { 
+                $set: { isLocked : false, updatedAt: new Date()  }
+
+            }
+        );
+        return NextResponse.json({ success: true, data: result }, { status: 200 });
+        
+    } catch (error) {
+        console.error('Error finding nearby fridges:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+
+}
